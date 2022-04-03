83b965a6160ae625a0e22ae380ca3545f205f1c1
Volodymyr Melnyk <volodymir.melnyk@gmail.com>
2022-04-04 01:21:24 +0300

    HW_8

--- HW_8/main.py
+++ HW_8/main.py
@@ -10,6 +10,10 @@ def create_congrats_shedule(users):
     # (сегдняшний исключаем)
     current_date = datetime.now().date()
     next_week_dates = [current_date + timedelta(days=i) for i in range(1, 8)]
+    # смотрим, какая дата в нашем 7-дневном календаре максимальная, чтобы потом проверить,
+    # не является ли Сб или Вс последними в нашем календаре; если являются, тогда мы не переносим
+    # поздравления на след. неделю (на новый понедельник)
+    max_date = max(next_week_dates)
 
     # создаем дефолтный словарь, в который будем добавлять пары
     # {'День недели':[список имен юзеров для поздравления]}
@@ -23,9 +27,15 @@ def create_congrats_shedule(users):
             if user['birthday'].month == date.month and user['birthday'].day == date.day:
                 # берем строчное представление дня недели рассматриваемой даты
                 weekday = date.strftime('%A')
-                # если ДР выпадает на выходной, то имя добавляем в список понедельника
-                if datetime.weekday(date) in [5, 6]:
+                weekday_num = datetime.weekday(date)
+
+                # если ДР выпадает на выходной в начале или середине календаря,
+                # то имя добавляем в список понедельника; если ДР выпадает на выходной в конце календаря,
+                # то не добавляем в список поздравлений
+                if weekday_num in [5, 6] and date < max_date - timedelta(days=1):
                     users_to_congrat['Monday'].append(user['name'])
+                elif weekday_num in [5, 6] and date >= max_date - timedelta(days=1):
+                    continue
                 else:
                     users_to_congrat[weekday].append(user['name'])
 
